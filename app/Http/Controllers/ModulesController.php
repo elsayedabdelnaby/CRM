d3fb7ff468cb008662e60d6a588b294acc74ae6e
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-02-17 16:48:30 +0200

    handle index and create functions for modules controller

--- app/Http/Controllers/ModulesController.php
+++ app/Http/Controllers/ModulesController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Module;
 
 class ModulesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $form_type  = 'insert';
+        $modules    = Module::all();
+        $module     = array();
+        return view('modules.modules', compact('form_type', 'modules', 'module'));
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return redirect('/modules');
+    }
 }

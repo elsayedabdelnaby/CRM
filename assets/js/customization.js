bee18d743f66da09d5d2cc12a35c8d40c29fd800
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-02-24 13:59:28 +0200

    finished from making governorates list dependent on countries list

--- assets/js/customization.js
+++ assets/js/customization.js
@@ -1,11 +1,29 @@
 /*console, alert, $*/
-$(document).ready(function() {
+function createOptionsList(list, data) {
+    'use strict';
+    $(list).empty();
+    $(list).append('<option value="0">Select</option>');
+    var index;
+    for (index = 0; index < data.length; index += 1) {
+        console.log(data[index]['name_en']);
+        $(list).append($('<option></option>')
+                .attr('value', data[index]['id'])
+                .text(data[index]['name_en'])
+                );
+    }
+}
+
+$(document).ready(function () {
     var countriesList = $('#countriesList');
     var governoratesList = $('#governoratesList');
     $(governoratesList).attr('disabled', 'disabled');
-    $(countriesList).change(function() {
+    $(countriesList).change(function () {
         if ($(countriesList).val() > 0) {
             $(governoratesList).removeAttr("disabled");
+            var url = '/the-king/api/countries/' + $(countriesList).val() + '/governorates';
+            $.get(url, function (data, status) {
+                createOptionsList(governoratesList, data);
+            });
         } else {
             $(governoratesList).attr('disabled', 'disabled');
         }
